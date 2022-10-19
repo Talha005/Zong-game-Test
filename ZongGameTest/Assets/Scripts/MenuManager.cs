73f52e8f88b4972b77103cb6cba2a7452d74f0d9
Talha Rashid <Talha.0801@live.com>
2022-10-19 23:20:52 +0500

    Commit MenuScript

---
+++ ZongGameTest/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public AudioSource BGmusic, iconmusic, selectmusic, treasuremusic;
+    public GameObject menuPanel, weaponpanel, pointspanel, instrumentspanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
